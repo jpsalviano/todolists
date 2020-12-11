0d371175b4fafddda134edfabda0f9499653f47a
joao <jpfsalviano@gmail.com>
2020-12-11 11:59:52 -0300

    user registration tests - gets user info from form

--- app.py
+++ app.py
@@ -2,24 +2,17 @@ import falcon
 import json
 
 
-class Status:
+class UserRegistration:
 	def on_get(self, req, resp):
-		resp.media = {"message": "ok"}
-		resp.status = falcon.HTTP_200
-
-
-class Register:
-	def on_get(self, req, resp):
-		resp.status = falcon.HTTP_200
+		pass
 
 	def on_post(self, req, resp):
-		resp.body = json.dumps(req.params)
+		resp.media = req.params
 
 
 def create():
 	app = api = falcon.API()
-	app.add_route("/status", Status())
-	app.add_route("/register", Register())
+	app.add_route("/register", UserRegistration())
 	return app
 
 app = create()
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,17 +12,11 @@ class TestApp(testing.TestCase):
         self.email = "john123@fake.com"
         self.password = "abc123-"
 
-    def test_app_status(self):
-    	doc = {"message": "ok"}
-    	result = self.simulate_get("/status")
-    	self.assertEqual(result.json, doc)
-    	self.assertEqual(result.status, HTTP_200)
-
-    def test_app_gets_register_page(self):
+    def test_user_registration_endpoint_status(self):
     	result = self.simulate_get("/register")
     	self.assertEqual(result.status, HTTP_200)
 
-    def test_app_gets_user_info_from_form(self):
+    def test_user_registration_gets_user_info_from_form(self):
     	user_info = {
     		"username": self.username,
     		"email": self.email,
@@ -31,3 +25,12 @@ class TestApp(testing.TestCase):
     	}
     	result = self.simulate_post("/register", params=user_info)
     	self.assertEqual(result.json, user_info)
+
+	def test_user_registration_validates_username(self):
+		pass
+
+	def test_user_registration_validates_password(self):
+		pass
+
+	def test_user_registration_validates_email(self):
+		pass
